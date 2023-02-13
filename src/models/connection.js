3101118020cebd77b5ff8acbd144a8378eee0454
Matheus Macrineu <macrineu.developer@gmail.com>
2023-02-13 14:40:23 -0300

    Add configuração da porta MySql

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,7 +1,10 @@
 const mysql = require('mysql2/promise');
 
+require('dotenv').config();
+
 const connection = mysql.createPool({
     host: process.env.DB_HOST,
+    port: 3307,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWD,
     database: process.env.DB_DATABASE
