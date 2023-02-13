20d6c6e398e95d4340d7ca7f2401a002ec54165c
Matheus Macrineu <macrineu.developer@gmail.com>
2023-02-13 11:54:42 -0300

    montando servidor a partir do arquivo de rotas

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (request, response) => response.status(200).send('Server is Running'));
-
-app.listen(3000, () => console.log('Server Running or port 3000'));
---
+++ src/server.js
@@ -0,0 +1,3 @@
+const app = require('./app')
+
+app.listen(3000, () => console.log('Server Running or port 3000'));
